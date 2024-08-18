3a28fdd44ba97c5d3671397cc1c566dd9b811b75
Md. Salauddin <salauddinahmed555@gmail.com>
2024-08-18 19:25:41 +0600

    added vercel api

--- src/Hooks/UseAxiosPublic.jsx
+++ src/Hooks/UseAxiosPublic.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosPublic = axios.create({
     
-    baseURL:"http://localhost:5000"
+    baseURL:"https://backend-ecommerce-tawny.vercel.app"
 })
 const useAxiosPublic = () => {
     return axiosPublic
